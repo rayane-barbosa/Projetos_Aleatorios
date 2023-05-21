b3ee4d42fbb064d1e3e5cb9c6b5342cf4f28e0d9
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-05-21 18:13:30 +0000

    projeto-react-converte-moeda

---
+++ Projetos-react/conversor-de-moedas/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormularioConversor from './components/Formulario.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <FormularioConversor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Projetos-react/conversor-de-moedas/src/components/Formulario.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+function FormularioConversor() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="number" placeholder="Digite o Valor a ser convertido" {...register("Digite o Valor a ser convertido", {required: true, maxLength: 80})} />
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default FormularioConversor;
