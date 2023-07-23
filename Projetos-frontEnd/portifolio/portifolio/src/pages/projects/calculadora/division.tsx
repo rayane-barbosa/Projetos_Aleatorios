871ae44287b2d7da20e0c2aa797e2a4250a299ac
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-07-23 22:41:03 +0000

    feature: basic-calculator-functions

---
+++ Projetos-frontEnd/portifolio/portifolio/src/pages/projects/calculadora/division.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const division = (a: number, b: number) => {
+    return a / b;
+}
+
+console.log(division(10, 2));
---
+++ Projetos-frontEnd/portifolio/portifolio/src/pages/projects/calculadora/multiplication.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const multiplication = (a: number, b: number) => {
+    return a * b;
+}
