267e84aad94e12b809f7d3883318084d459343a2
rayane-barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-07-23 22:41:03 +0000

    feature: basic-calculator-functions

---
+++ Projetos-frontEnd/portifolio/portifolio/src/pages/projects/calculadora/subtraction.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const subtraction = (a: number, b: number) => {
+    return a - b;
+}
---
+++ Projetos-frontEnd/portifolio/portifolio/src/pages/projects/calculadora/sum.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Sum = (a: number, b: number) => {
+    return a + b;
+}
